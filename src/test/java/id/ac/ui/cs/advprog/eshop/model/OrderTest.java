ef92e37fc1b53a906e51ce6fa2c9e15c60de4913
ArditheusIH <arditheus.immanuel@gmail.com>
2024-03-06 16:26:10 +0700

    [RED] Add Order model skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -72,7 +72,6 @@ public class OrderTest {
     void testSetStatusToCancelled(){
         Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
                 this.products, 1708560000L, "Safira Sudirajat", "SUCCESS");
-        assertEquals("SUCCESS", order.getStatus());
         order.setStatus("CANCELLED");
         assertEquals("CANCELLED", order.getStatus());
     }
